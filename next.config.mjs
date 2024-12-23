57815460616c4bcba9e1840cc37b284bdfe66f86
webency <webency@mail.ru>
2024-12-24 00:55:17 +0300

    Tatata

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: '/api/v1/:path*', // for local development
-        // for docker use http://{backend_container}:port/api/v1/:path*
-      },
-    ];
-  },
   output: 'standalone',
   webpack(config) {
     // Grab the existing rule that handles SVG imports
