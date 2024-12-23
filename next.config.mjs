53c2b2785580965095b3cfd601b60772616d63d8
webency <webency@mail.ru>
2024-12-24 00:29:56 +0300

    change path

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:5000/api/v1/:path*', // for local development
+        destination: 'http://frontend_game/api/v1/:path*', // for local development
         // for docker use http://{backend_container}:port/api/v1/:path*
       },
     ];
