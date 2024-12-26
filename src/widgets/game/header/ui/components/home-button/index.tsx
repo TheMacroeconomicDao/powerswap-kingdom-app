e036e8f01e3194ab552872892c1d911f431b7239
Josiren <egor.novikov666@bk.ru>
2024-12-27 01:18:31 +0300

    add: animation for queen

--- src/widgets/game/header/ui/components/home-button/index.tsx
+++ src/widgets/game/header/ui/components/home-button/index.tsx
@@ -11,7 +11,9 @@ import styles from '../../styles/header.module.css';
 export const HomeButton = () => {
   return (
     <motion.button
-      initial={{ scale: 1 }}
+      initial={{ scale: 0.7, translateX: '-8.2em', opacity: 0 }}
+      transition={{ duration: 1, delay: 0.2 }}
+      animate={{ scale: 1, translateX: '0', opacity: 1 }}
       whileTap={{ scale: 0.92, transition: { duration: 0.02, ease: 'easeInOut' } }}
       onClick={() => {
         setTab('none');
