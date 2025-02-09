ffb68e904a3a6a93e5284569a3a5e72eb0869b4e
Fuad010 <dezhurik.com@gmail.com>
2025-02-09 18:31:12 +0400

    dev:feature-upgrades

--- src/widgets/game/components/Header/ui/components/home-button/ui/index.tsx
+++ src/widgets/game/components/Header/ui/components/home-button/ui/index.tsx
@@ -11,11 +11,10 @@ import { useTranslation } from 'react-i18next';
 export const HomeButton = () => {
   const { t } = useTranslation('translation', { keyPrefix: 'game.home.navbar' });
   return (
-    <div className='relative'>
       <LinkButton
         transition={{ duration: 1, delay: 0.2 }}
         whileTap={{ scale: 0.92, transition: { duration: 0.02, ease: 'easeInOut' } }}
-        className="justify-center"
+        className="relative justify-center"
         onClick={() => {
           setTab('none');
         }}
@@ -23,8 +22,7 @@ export const HomeButton = () => {
         <div className="size-[72px]">
           <Queen />
         </div>
+        <h6 className='absolute bottom-[-20px] text-[14px] mx-auto left-0 right-0 w-fit'>{t('home.text')}</h6>
       </LinkButton>
-      <h6 className='absolute text-[14px] mx-auto left-0 right-0 w-fit'>{t('home.text')}</h6>
-    </div>
   );
 };
